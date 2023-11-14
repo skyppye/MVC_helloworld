ad2a62ff92a26aad2a56f0096a8d0b681d789da7
skyppye2002 <skyppye2002@gmail.com>
2023-11-14 20:48:42 +0200

    commit initial

---
+++ MVCdemo/src/main/java/MVC/com/example/MVCdemo/MvCdemoApplication.java
@@ -0,0 +1,13 @@
+package MVC.com.example.MVCdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvCdemoApplication.class, args);
+	}
+
+}
